ec6b167b48a9ec58646131d9fae68f5cfc34ec60
ahw22 <andreas.zincke@gmx.at>
2024-10-07 13:37:02 +0200

    initialize repo

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.util.zip.ZipEntry;
 
 public class Main {
     public static void main(String[] args) {
-    System.out.println("HelloWorld");
+    System.out.println("Hello World");
 
 
 
